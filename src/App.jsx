4f620c0127f82e8926b71f8586f48e265bd24794
ANDREYDEN <andrey2850@gmail.com>
2021-03-04 13:10:59 -0500

    Add user state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import './App.css';
 
-function renderMessage(message) {
+function renderMessage(message, currentUser) {
+  const isMyMessage = (currentUser !== null) && (currentUser.uid === message.author?.uid)
   return (
-    <div key={message.id}>
+    <div key={message.id} className={isMyMessage ? 'my-message' : 'their-message'}>
       <h2>{message.content}</h2>
-      <p>{message.author}</p>
+      <p>{message.author?.userName || 'someone'}</p>
     </div>
   )
 }
@@ -13,6 +14,7 @@ function renderMessage(message) {
 function App() {
   const [messages, setMessages] = useState([])
   const [currentMessageContent, setCurrentMessageContent] = useState('')
+  const [user, setUser] = useState(null)
 
   const updateMessage = (e) => {
     setCurrentMessageContent(e.target.value)
@@ -22,18 +24,38 @@ function App() {
     const newMessage = {
       id: messages.length,
       content: currentMessageContent,
-      author: 'someone'
+      author: user
     }
     setMessages([...messages, newMessage])
   }
 
+  const handleLogIn = () => {
+    setUser({
+      uid: '1234qwer',
+      userName: 'user3000'
+    })
+  }
+
+  const handleLogOut = () => {
+    setUser(null)
+  }
 
   return (
     <div className="App">
       <h1>Let's talk about Solution Challenge</h1>
-      <input type="text" onChange={updateMessage}/>
+      {
+        user
+          ? <div>
+            Logged in as {user.userName}
+            <br />
+            <button onClick={handleLogOut}>Log Out</button>
+          </div>
+          : <button onClick={handleLogIn}>Log In with Google</button>
+      }
+      <br />
+      <input type="text" onChange={updateMessage} />
       <button onClick={sendMessage}>Send</button>
-      { messages.map(renderMessage)}
+      { messages.map(message => renderMessage(message, user))}
     </div>
   );
 }
